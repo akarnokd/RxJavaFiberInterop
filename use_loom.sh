f214ae93d21a6b879afb924d03c4ab3a36e6c76a
David Karnok <akarnokd@gmail.com>
2022-12-01 09:41:49 +0100

    Update use_loom.sh

--- use_loom.sh
+++ use_loom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://download.java.net/java/early_access/jdk19/22/GPL/openjdk-19-ea+22_linux-x64_bin.tar.gz
-tar -zxvf openjdk-19-ea+22_linux-x64_bin.tar.gz
-export LOOM_JDK=jdk-19
+wget https://download.java.net/java/early_access/loom/20/openjdk-20-loom+20-40_linux-x64_bin.tar.gz
+tar -zxvf openjdk-20-loom+20-40_linux-x64_bin.tar.gz
+export LOOM_JDK=jdk-20
 eval "$LOOM_JDK/bin/java -version"
 echo "LOOM_JDK=$LOOM_JDK" >> $GITHUB_ENV
