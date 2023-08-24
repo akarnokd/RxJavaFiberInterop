693ce38cfa633d65f20de3bc9a011017392175e3
David Karnok <akarnokd@gmail.com>
2023-08-24 07:04:50 +0200

    Update use_loom.sh

--- use_loom.sh
+++ use_loom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://download.java.net/java/early_access/jdk21/26/GPL/openjdk-21-ea+26_linux-x64_bin.tar.gz
-tar -zxvf openjdk-21-ea+26_linux-x64_bin.tar.gz
+wget https://download.java.net/java/GA/jdk21/fd2272bbf8e04c3dbaee13770090416c/35/GPL/openjdk-21_linux-x64_bin.tar.gz
+tar -zxvf openjdk-21_linux-x64_bin.tar.gz
 export LOOM_JDK=jdk-21
 eval "$LOOM_JDK/bin/java -version"
 echo "LOOM_JDK=$LOOM_JDK" >> $GITHUB_ENV
